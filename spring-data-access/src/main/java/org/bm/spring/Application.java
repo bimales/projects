26982c1742e6b2e0150f2c0325890674c40dd15e
bimales <bimales@live.in>
2016-02-23 01:06:09 +0530

    updated with simpleJdbcTemplate

--- spring-data-access/src/main/java/org/bm/spring/Application.java
+++ spring-data-access/src/main/java/org/bm/spring/Application.java
@@ -2,17 +2,17 @@ package org.bm.spring;
 
 import java.sql.SQLException;
 
-import org.bm.spring.dao.JdbcDaoImpl;
+import org.bm.spring.dao.PersonsDao;
 import org.bm.spring.pojo.Person;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(final String args[]) {
-		final ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		final JdbcDaoImpl dao = context.getBean("jdbcDaoImpl", JdbcDaoImpl.class);
+		final ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		final PersonsDao personsDaoImpl = context.getBean("personsDao", PersonsDao.class);
 		try {
-			final Person person = dao.getPersonById("1");
+			final Person person = personsDaoImpl.getPersonById("1");
 			System.out.println("Person Details: \n" + person.toString());
 		} catch (final SQLException e) {
 			e.printStackTrace();
--- spring-data-access/src/main/java/org/bm/spring/dao/PersonsDao.java
+++ spring-data-access/src/main/java/org/bm/spring/dao/PersonsDao.java
@@ -1,36 +1,33 @@
 package org.bm.spring.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.sql.DataSource;
-
 import org.bm.spring.pojo.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JdbcDaoImpl {
+public class PersonsDao {
 
 	@Autowired
-	private DataSource dataSource;
+	private SimpleJdbcTemplate simpleJdbcTemplate;
 	private static final String GET_PERSON_BY_ID = "select * from persons where personId =?";
 
 	public Person getPersonById(final String id) throws SQLException {
-		final Person person = new Person();
-		final Connection conn = dataSource.getConnection();
-		final PreparedStatement ps = conn.prepareStatement(GET_PERSON_BY_ID);
-		ps.setInt(1, Integer.parseInt(id));
-		final ResultSet rs = ps.executeQuery();
-		if (rs.next()) {
+		return simpleJdbcTemplate.queryForObject(GET_PERSON_BY_ID, new PersonsRowMapper(), new Object[] { id });
+	}
+
+	public class PersonsRowMapper implements RowMapper<Person> {
+		public Person mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+			final Person person = new Person();
 			person.setLastName(rs.getString("lastName"));
 			person.setFirstName(rs.getString("firstName"));
 			person.setAddress(rs.getString("address"));
 			person.setCity(rs.getString("city"));
+			return person;
 		}
-		return person;
 	}
-
 }
