52296c13c16fdc761082ffaa4a234ad26eeac51c
bimales <mail2bimales@gmail.com>
2017-05-16 17:26:48 +0530

    blank commit

---
+++ simple-quiz/src/main/java/org/bm/test/App.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package org.bm.test;
+
+/**
+ * @author bimales
+ *
+ */
+public class App {
+
+	/**
+	 * 
+	 */
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
